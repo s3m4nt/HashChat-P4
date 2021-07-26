bc683bd416b329a1c059d4d41a8b4b1ea41fd548
Brian Young <berkshirehunt@gmail.com>
2021-07-25 20:05:44 -0700

    logging msg to console

--- src/components/ChatScreen.jsx
+++ src/components/ChatScreen.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import { useRealtime } from 'react-supabase'
+import React, { useState } from 'react'
+import { useRealtime, useInsert } from 'react-supabase'
 
 function ChatScreen(props) {
 
+    const [inputTxt, setInputTxt] = useState('')
     const [result, reexecute] = useRealtime('messages')
+    const [insertResponse, insertMessage] = useInsert('messages')
     const { data, fetching, error } = result
-  
+
     if (fetching || !data) return <p>Loading...</p>
     if (error) return <p>Oh no... {error.message}</p>
 console.log(data)
@@ -15,7 +17,30 @@ console.log(data)
                 {props.userName}
                 <button onClick={props.onLogOut}>logout</button>
             </div>
+            <div>
+            <form onSubmit={async (e) => {
+            e.preventDefault()
+            if (inputTxt === '') {
+                alert('message cant be empty')
+                return
+            }
+            
+            // props.onLogIn(inputTxt)
+            console.log(inputTxt)
+            const {error} = await insertMessage({ username: props.userName, text: inputTxt })
+            setInputTxt('') 
+            if (error) {
+                console.log(error)
+            }
+        }}>
+           <input type="text" value={inputTxt} placeholder="" onChange={(e) => {
+               setInputTxt(e.target.value)
+           }} />
+           <input type="submit" value="submit/login" />
+        </form>
+            </div>
             <ul>
+            {/* // li the message and username */}
                 {data.map((message) =>{
                     return <li key={message.id}>{message.username} : {message.text}</li>
                 })}
